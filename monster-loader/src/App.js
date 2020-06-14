c70b62296fbdd27d8e6cd262ec9d5fe811a6cade
Wen Jie <wenjiens15@gmail.com>
2020-06-15 01:06:33 +0800

    class component - setState

--- monster-loader/src/App.js
+++ monster-loader/src/App.js
@@ -3,22 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      string: 'Hello wen jie'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.state.string}</p>
+          <button onClick = {() => this.setState({string: 'hellow ling'})}>
+            Change text
+          </button>
         </header>
       </div>
     );
