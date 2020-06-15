fde76f5be7c7f9837b47a2d6a42eb19329eae60f
Wen Jie <wenjiens15@gmail.com>
2020-06-16 02:32:47 +0800

    fetching content

--- monster-loader/src/App.js
+++ monster-loader/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +6,25 @@ class App extends Component {
     super()
 
     this.state = {
-      string: 'Hello wen jie'
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({monsters: users}))
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick = {() => this.setState({string: 'hellow ling'})}>
-            Change text
-          </button>
+          {
+            this.state.monsters.map((monster) => 
+              <h1 key={monster.id}>{monster.name}</h1>
+            )
+          }
         </header>
       </div>
     );
