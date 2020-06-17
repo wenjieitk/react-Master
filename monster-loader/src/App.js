2bbead3ef65347ed36fa21fcf2d1b827ffd2b2b7
Wen Jie <wenjiens15@gmail.com>
2020-06-18 04:35:54 +0800

    search component

--- monster-loader/src/App.js
+++ monster-loader/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Cardlist} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component.jsx'
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -18,9 +20,19 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    })
     return (
       <div className="App">
-        <Cardlist monsters = {this.state.monsters} />
+        <SearchBox 
+          placeholder = 'search monster'
+          handleChange = {
+            e => this.setState({searchField: e.target.value}) //, () => console.log(this.state) <--- do something after the event changes in the callBack
+          }
+        />
+        <Cardlist monsters = {filteredMonsters} />
       </div>
     );
   }
